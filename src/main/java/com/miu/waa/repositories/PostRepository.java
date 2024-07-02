945fb40e2eeacb98cb1288ebd66c61b2681be4ca
peter-hoangquochung <161963090+peter-hoangquochung@users.noreply.github.com>
2024-07-02 13:44:44 -0500

    commit

--- src/main/java/com/miu/waa/repositories/PostRepository.java
+++ src/main/java/com/miu/waa/repositories/PostRepository.java
@@ -2,11 +2,12 @@ package com.miu.waa.repositories;
 
 import com.miu.waa.entities.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface PostRepository extends JpaRepository<Post, Long> {
+public interface PostRepository extends JpaRepository<Post, Long>, JpaSpecificationExecutor<Post> {
     List<Post> findByThreadIdOrderByCreatedAtAsc(Long threadId);
 }
